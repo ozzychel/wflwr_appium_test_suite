3e952f029cebf91a0828bba58b1d2ef80cb1ed02
Andrey Petunin <andrey.petunin86@gmail.com>
2023-07-07 09:13:14 -0700

    create helper function to get list of all connected devices

---
+++ appTest/helpers/getDevices.js
@@ -0,0 +1,82 @@
+const { exec } = require('child_process');
+
+function getConnectedAndroidDevices() {
+  return new Promise((resolve, reject) => {
+    exec('adb devices', (error, stdout, stderr) => {
+      if (error) {
+        reject(error);
+        return;
+      }
+
+      const deviceList = [];
+
+      const lines = stdout.trim().split('\n');
+
+      const promises = [];
+
+      for (let i = 1; i < lines.length; i++) {
+        const line = lines[i].trim();
+
+        if (line.includes('\tdevice')) {
+          const deviceInfo = {};
+
+          const udid = line.split('\t')[0];
+          deviceInfo.udid = udid;
+
+          const deviceNamePromise = getDeviceProperty(udid, 'ro.product.model');
+          const androidVersionPromise = getDeviceProperty(udid, 'ro.build.version.release');
+
+          promises.push(
+            Promise.all([deviceNamePromise, androidVersionPromise])
+              .then(([deviceName, androidVersion]) => {
+                deviceInfo.deviceName = deviceName;
+                deviceInfo.platformName = 'Android';
+                deviceInfo.platformVersion = androidVersion;
+                deviceList.push(deviceInfo);
+              })
+              .catch(error => {
+                console.error('Error:', error);
+              })
+          );
+        }
+      }
+
+      Promise.all(promises)
+        .then(() => {
+          resolve(deviceList);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  });
+}
+
+function getDeviceProperty(udid, property) {
+  return new Promise((resolve, reject) => {
+    exec(`adb -s ${udid} shell getprop ${property}`, (error, stdout, stderr) => {
+      if (error) {
+        reject(error);
+        return;
+      }
+      resolve(stdout.trim());
+    });
+  });
+}
+
+// Usage example
+// getConnectedAndroidDevices()
+//   .then(devices => {
+//     // console.log(devices);
+//     return devices;
+//   })
+//   .catch(error => {
+//     console.error('Error:', error);
+//   });
+
+async function test () {
+  const devices = await getConnectedAndroidDevices();
+  console.log("WORKS:", devices)
+}
+
+test()
